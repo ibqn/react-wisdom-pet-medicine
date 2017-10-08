dbe2207f8f9015f0cf79ab92d3e6369270e28755
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-10-08 10:03:50 +0200

    fetch json data

--- src/components/MainInterface.jsx
+++ src/components/MainInterface.jsx
@@ -2,9 +2,41 @@ import React from "react"
 
 
 export class MainInterface extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { appointments : [] }
+    }
+
+    componentDidMount() {
+        console.log('did mount')
+        fetch('http://localhost:3000/data.json', { method: 'get' })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ appointments : json })
+                console.log(`data ${this.state.appointments}`)
+            })
+    }
+
     render() {
+        const filteredApts = this.state.appointments.map((item, index) => {
+            return (
+                <li className="pet-item media" key={index}>
+                    <div className="pet-info media-body">
+                        <div className="pet-head">
+                            <span className="pet-name">{item.petName}</span>
+                            <span className="apt-date pull-right">{item.aptDate}</span>
+                        </div>
+                        <div className="owner-name"><span className="label-item">Owner: </span>
+                        {item.ownerName}</div>
+                        <div className="apt-notes">{item.aptNotes}</div>
+                    </div>
+                </li>
+            )
+        })
         return (
-            <h1>Pets Appointments</h1>
+            <div className="interface">
+                <ul className="item-list media-list">{filteredApts}</ul>
+            </div>
         )
     }
 }
