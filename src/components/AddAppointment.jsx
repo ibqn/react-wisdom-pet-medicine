8f567d7e699e846cec0c4d84d73f80032042ea35
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-10-09 09:25:38 +0200

    add onchange for new apts

--- src/components/AddAppointment.jsx
+++ src/components/AddAppointment.jsx
@@ -4,14 +4,30 @@ import React from "react"
 export class AddAppointment extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            inputPetName: '',
+            inputOwnerName: '',
+            inputAptDate: '',
+        }
     }
 
+    onDataChange = (input) => this.setState({ [input.target.name]: input.target.value })
+
     onDisplayToggle = () => this.props.onDisplayToggle()
+
+    onSubmit = (event) => {
+        event.preventDefault()
+    }
 
     render() {
         const displayBody = {
             display: this.props.displayBody? 'block' : 'none'
         }
+        const inputPetName = this.state.inputPetName
+        const inputOwnerName = this.state.inputOwnerName
+        const inputAptDate = this.state.inputAptDate
+        const inputAptTime = this.state.inputAptTime
         return (
             <div className="panel panel-primary">
                 <div className="panel-heading apt-addheading" onClick={this.onDisplayToggle}>
@@ -19,31 +35,40 @@ export class AddAppointment extends React.Component {
                     Add Appointment
                 </div>
                 <div className="panel-body" style={displayBody}>
-                    <form className="add-appointment form-horizontal">
+                    <form className="add-appointment form-horizontal"
+                        onSubmit={this.onSubmit}>
                         <div className="form-group">
                             <label className="col-sm-2 control-label" htmlFor="petName">Pet Name</label>
                             <div className="col-sm-10">
                                 <input type="text" className="form-control"
-                                    id="petName" ref="inputPetName" placeholder="Pet's Name" />
+                                    name="inputPetName"
+                                    value={inputPetName} onChange={this.onDataChange}
+                                    id="petName" placeholder="Pet's Name" />
                             </div>
                         </div>
                         <div className="form-group">
                             <label className="col-sm-2 control-label" htmlFor="petOwner">Pet Owner</label>
                             <div className="col-sm-10">
                                 <input type="text" className="form-control"
-                                    id="petOwner" ref="inputOwnerName" placeholder="Owner's Name" />
+                                    name="inputOwnerName"
+                                    value={inputOwnerName} onChange={this.onDataChange}
+                                    id="petOwner" placeholder="Owner's Name" />
                             </div>
                         </div>
                         <div className="form-group">
                             <label className="col-sm-2 control-label" htmlFor="aptDate">Date</label>
                             <div className="col-sm-4">
                                 <input type="date" className="form-control"
-                                    id="aptDate" ref="inputAptDate" />
+                                    name="inputAptDate"
+                                    value={inputAptDate} onChange={this.onDataChange}
+                                    id="aptDate" />
                             </div>
                             <label className="col-sm-2 control-label" htmlFor="aptTime">Time</label>
                             <div className="col-sm-4">
                                 <input type="time" className="form-control"
-                                    id="aptTime" ref="inputAptTime" />
+                                    name="inputAptTime"
+                                    value={inputAptTime} onChange={this.onDataChange}
+                                    id="aptTime" />
                             </div>
 
                         </div>
